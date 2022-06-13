4e7e8b25ef93fb485288ce0c260d61bcae1a6706
Noam <noam.moshe@campus.technion.ac.il>
2022-06-13 21:03:14 +0300

    added 1-5 steps

--- hw4.c
+++ hw4.c
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-#define SH_SYMTAB 2
+#define UND 0
 
 pid_t run_target(const char* programname)
 {
@@ -191,7 +191,7 @@ void run_counter_debugger(pid_t child_pid)
 int isExecutable(char* file_name)
 {
     int is_elf_file = 0;
-    void* file;
+    char* file;
     FILE* fd = fopen(file_name, "r");
     char* buffer = malloc(sizeof(char) * 5);
     int read_bytes = fread(buffer, 1, 4, fd);
@@ -220,7 +220,7 @@ int isExecutable(char* file_name)
     return is_elf_file;
 }
 
-int isSymbol(char* file_name, char* symbol)
+void isSymbol(char* file_name, char* symbol, int* symbol_found, int* is_global, int* is_defined, Elf64_Addr* symbol_address)
 {
     FILE* fd = fopen(file_name, "r");
     if (fd == NULL)
@@ -230,30 +230,130 @@ int isSymbol(char* file_name, char* symbol)
         exit(1);
     Elf64_Ehdr* elf_header = (Elf64_Ehdr*)file;
     Elf64_Shdr* sections = (Elf64_Shdr*)(file + elf_header->e_shoff);
-    Elf64_Shdr* str_section = (Elf64_Shdr*)(sections + elf_header->e_shstrndx);
-    char* str_tbl = file + str_section->sh_offset;
 
-    Elf64_Sym* symtab;
-    char *strtab;
-    for (int i = 0; i < header->e_shnum; i++)
-        if (sections[i].sh_type == SHT_SYMTAB) {
-//            todo: add !strcmp(".symtab", section_name) ||?
-            symtab = (Elf64_Sym*)((char*)file + sections[i].sh_offset);
-            break; }
-    fclose(fd);
+    int num_of_symbols = 0, num_of_realocs = 0;
+    Elf64_Shdr section_header_str_section = sections[elf_header->e_shstrndx];
+    char* section_header_str_tbl = file + section_header_str_section.sh_offset;
+    char *strtab = NULL;
+    Elf64_Sym* symtab = NULL;
+    Elf64_Sym* dynsym = NULL;
+    Elf64_Rela* plt_relocation_table = NULL;
+
+    for (i = 0; i < elf_header->e_shnum; i++){
+        if (strcmp(".symtab", section_header_str_tbl + sections[i].sh_name) == 0) {
+            symtab = (Elf64_Sym*)(file + sections[i].sh_offset);
+            num_of_symbols = sections[i].sh_size / sections[i].sh_entsize;
+            break;
+        }
+        else if(strcmp(".strtab", section_header_str_tbl + sections[i].sh_name) == 0){
+            strtab = (file + sections[i].sh_offset);
+        }
+        else if(strcmp(".dynsym", section_header_str_tbl + sections[i].sh_name) == 0){
+            dynsym = (Elf64_Rela*)(file + sections[i].sh_offset);
+        }
+        else if(strcmp(".rela.plt", section_header_str_tbl + sections[i].sh_name) == 0){
+            plt_relocation_table = (Elf64_Rela*)(file + sections[i].sh_offset);
+            num_of_realocs = sections[i].sh_size / sections[i].sh_entsize;
+        }
+    }
+    if (num_of_symbols == 0 || symtab == NULL || strtab == NULL)
+        return;
+    for(i = 0; i < num_of_symbols ; i++)
+    {
+        if(strcmp(symbol, strtab + symtab[i].st_name) == 0) {
+            *symbol_found = 1;
+            if(ELF64_ST_BIND(symtab[i].st_info) == GLOBAL)
+                *is_global = 1;
+            if(symtab[i].st_shndx != UND) {  //todo: check if value of UND is indeed 0
+                *is_defined = 1;
+                *symbol_address	= symtab[i].st_value;
+            }
+        }
+    }
+    if(*is_defined == 1)
+    {
+        if(fclose(fd) == NULL)
+            exit(1);
+    }
+
+    for(i = 0; i < num_of_realocs ; i++)
+    {
+        Elf64_Sym dynsym_entry = dynsym + ELF64_R_SYM(plt_relocation_table[i]);
+        if(strcmp(symbol, strtab + dynsym_entry.st_name) == 0) {
+            &symbol_address = plt_relocation_table[i].r_offset;
+        }
+    }
+    if(fclose(fd) == NULL)
+        exit(1);
+}
+
+findSymbolPosition(char* file_name, char* symbol, Elf64_Addr* symbol_address)
+{
+    FILE* fd = fopen(file_name, "r");
+    if (fd == NULL)
+        exit(1);
+    char* file = mmap(NULL, lseek(fd, 0, SEEK_END), PROT_READ, MAP_PRIVATE, fd, 0);
+    if (file == MAP_FAILED)
+        exit(1);
+    Elf64_Ehdr* elf_header = (Elf64_Ehdr*)file;
+    Elf64_Shdr* sections = (Elf64_Shdr*)(file + elf_header->e_shoff);
+
+    Elf64_Shdr section_header_str_section = sections[elf_header->e_shstrndx];
+    char* section_header_str_tbl = file + section_header_str_section.sh_offset;
+    char *strtab = NULL;
+    Elf64_Sym* symtab = NULL;
+    Elf64_Rela* text_relocation_table = NULL;
+    int num_of_realocs = 0, i;
+
+    for (i = 0; i < elf_header->e_shnum; i++){
+        if (strcmp(".symtab", section_header_str_tbl + sections[i].sh_name) == 0) {
+            symtab = (Elf64_Sym*)(file + sections[i].sh_offset);
+            break;
+        }
+        else if(strcmp(".rela.text", section_header_str_tbl + sections[i].sh_name) == 0){
+            text_relocation_table = (Elf64_Rela*)(file + sections[i].sh_offset);
+            num_of_realocs = sections[i].sh_size / sections[i].sh_entsize;
+        }
+        else if(strcmp(".strtab", section_header_str_tbl + sections[i].sh_name) == 0){
+            strtab = (file + sections[i].sh_offset);
+        }
+    }
+    if (num_of_realocs == 0 || symtab == NULL || text_relocation_table == NULL || strtab == NULL)
+        return;
+    for(i = 0; i < num_of_realocs ; i++)
+    {
+        Elf64_Sym symtab_entry = symtab + ELF64_R_SYM(text_relocation_table[i]);
+        if(strcmp(symbol, strtab + symtab_entry.st_name) == 0) {
+            *symbol_found = 1
+            if(ELF64_ST_BIND(symtab[i].st_info) == GLOBAL) {
+                *is_global = 1;
+            }
+        }
+    }
+    if(fclose(fd) == NULL)
+        exit(1);
+    return;
 }
 
 int main(int argc, char** argv)
 {
     pid_t child_pid;
+    int symbol_is_found = 0, symbol_is_global = 0, symbol_is_defined = 0;
+    Elf64_Addr symbol_address;
     if(isExecutable(argv[1]) == 0)
     {
         printf("PRF:: %s not an executable! :(\n", argv[1]);
         return 0;
     }
-    if(isSymbol(argv[1], argv[0]) == 0)
+    isSymbol(argv[1], argv[0], &symbol_is_found, &symbol_is_global, &symbol_is_defined, &symbol_address);
+    if(symbol_is_found == 0)
     {
         printf("PRF:: %s not found!\n", argv[0]);
+        return 0;
+    }
+    if(symbol_is_global == 0)
+    {
+        printf("PRF:: <function name> is not a global symbol! :(\n", argv[0]);
         return 0;
     }
     child_pid = run_target(argv[1]);
